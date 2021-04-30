360c432c96eafd9443a8155f74cfb78f80928ea0
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2021-04-30 17:15:33 +0300

    animaatiot

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     
-    public float nopeus = 5.0f;
+    public float speed = 5.0f;
 
     private float vertikaalinenPyorinta = 0;
     private float horisontaalinenPyorinta = 0;
     private float xRotation = 0f;
 
-    public float hyppyvoima = 30f;
-    public float painovoima = 5f;
+    public float hyppyvoima = 0f;
+    public float painovoima = 0f;
 
-    public Transform groundCheck;
-    public LayerMask groundMask;
-    public float groundDistance = 0.4f;
-    private bool isGrounded;
+    private bool isGrounded = true;
 
     public Animator anim;
 
@@ -31,49 +28,84 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
         CharacterController hahmokontrolleri = GetComponent<CharacterController>();
-        float horizontal = Input.GetAxis("Horizontal") * 5;
-        float vertical = Input.GetAxis("Vertical") * 5;
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
         Vector3 nopeus = new Vector3(horizontal, 0, vertical);
 
         horisontaalinenPyorinta += Input.GetAxis("Mouse X") * 3;
         vertikaalinenPyorinta -= Input.GetAxis("Mouse Y") * 3;
 
-        xRotation = vertikaalinenPyorinta;
-        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
-
         transform.localRotation = Quaternion.Euler(0, horisontaalinenPyorinta, 0);
 
         nopeus = transform.rotation * nopeus;
 
-        hahmokontrolleri.SimpleMove(nopeus);
+       
 
-        if (Input.GetKeyDown("space") && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            print("space key was pressed");
-            nopeus.y = hyppyvoima;
+            Debug.Log("hyppy");
+
+            nopeus.y += hyppyvoima;
+            anim.SetBool("JumpU", true);
+        }
+        else
+        {
+            anim.SetBool("JumpU", false);
         }
 
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("hyppy");
+
+            nopeus.y += hyppyvoima;
+            anim.SetBool("JumpR", true);
+        }
+        else
+        {
+            anim.SetBool("JumpR", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Debug.Log("hyppy");
+
+            nopeus.y += hyppyvoima;
+            anim.SetBool("JumpL", true);
+        }
+        else
+        {
+            anim.SetBool("JumpL", false);
+        }
+
+
+
+
+        nopeus.y = nopeus.y - painovoima * Time.deltaTime;
+
+        hahmokontrolleri.Move(nopeus * Time.deltaTime);
+
+
         if (Input.GetAxis("Vertical") != 0)
         {
             anim.SetBool("Walk", true);
+
+            if (Input.GetKeyDown("left shift"))
+            {
+                anim.SetBool("Run", true);
+                anim.SetBool("Walk", false);
+            }
+            
         }
         else
         {
             anim.SetBool("Walk", false);
+            anim.SetBool("Run", false);
         }
 
-        nopeus.y -= painovoima * Time.deltaTime;
-        hahmokontrolleri.Move(nopeus * Time.deltaTime);
+        
 
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
-    }
+    
 }
